87d52617811d370becb5b39b88367fbae018254a
Andres Morera <cristian.morera89@gmail.com>
2026-05-04 14:14:45 -0500

    test: add e2e checkout test with screenshots

--- config/routes.ts
+++ config/routes.ts
@@ -1,4 +1,5 @@
 import { E2E_BASE_URL } from './url';
 
 export const E2E_INVENTORY_PATH = '/inventory.html';
+export const E2E_CART_PATH = '**/checkout-complete.html';
 export const E2E_INVENTORY_URL = `${E2E_BASE_URL}${E2E_INVENTORY_PATH}`;
--- pages/OrderConfirmationPage.ts
+++ pages/OrderConfirmationPage.ts
@@ -1,12 +1,13 @@
 import { Page, TestInfo } from '@playwright/test';
 import { attachPageScreenshot } from '../utils/ScreenshotReporter';
+import { E2E_CART_PATH } from '../config/routes';
 
 export class OrderConfirmationPage {
 	constructor(private readonly page: Page) {}
 
 	async finishOrder(testInfo?: TestInfo): Promise<void> {
 		await this.page.locator('[data-test="finish"]').click();
-		await this.page.waitForURL('**/checkout-complete.html');
+		await this.page.waitForURL(E2E_CART_PATH);
 		if (testInfo) {
 			await attachPageScreenshot(this.page, testInfo, 'order-confirmation-opened');
 		}
